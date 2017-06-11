4fdfa06223848834949a7231d57e60fe351aace3
kld <kldgarrido.com>
2017-06-11 12:07:39 -0500

    add bootstrap and product#index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'products/index'
 
-  get 'products/edit'
+  root 'products#index'
+
+  resources 'products'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
