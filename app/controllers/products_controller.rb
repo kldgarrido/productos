ef3172b91ecefb69585864eb5aa7b78ceb5f845d
kld <kldgarrido.com>
2017-06-13 06:27:06 -0500

    add create function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,49 @@
 class ProductsController < ApplicationController
-  def index
-  	@products = Product.all
-  end
 
-  def edit
-  	@product = Product.find(params[:id])
-  	@categories_select = @product.category
-  	@categories = Category.all
-  end
+	def index
+		@products = Product.all
+	end
+
+
+	def new
+		@product = Product.new
+		@categories = Category.all
+	end
+
+
+	def edit
+		@product = Product.find(params[:id])
+		@categories = Category.all
+	end
+
+
+	def create
+	  @product = Product.new(product_params)
+	  #@product.user = current_user
+	  if @product.save
+	    redirect_to products_path, notice: "El producto fue publicado con éxito"
+	  else
+	    render :new
+	  end
+	end
+
+
+	def update
+	  @product = Product.find(params[:id])
+	  
+	  if @product.update(product_params)
+	    redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+	  else
+	    render :edit
+	  end
+	end
+
+
+	private
+	  def product_params
+	    #params.require(:product).permit(:name, :price, :category  => [])
+	    params.require(:product).permit(:name, :price, :category)
+	  end
+
+
 end
